aa2293ffc9fd1335db9435bd7a1f6f16bc976a7b
adamclear <3589@holbertonschool.com>
2022-03-18 21:35:48 +0000

    updated storage engines for task 7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -64,3 +64,9 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         self.__session = scoped_session(sessionmaker(bind=self.__engine,
                                                      expire_on_commit=False))
+
+    def close(self):
+        """
+        
+        """
+        self.__session.remove()
